e5a5c392f97152f48d0314b4e4aed7062a6cb093
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2025-01-08 07:33:23 -0300

    test: improve partition tests

--- service/internal/preprocessor/partition/partition.go
+++ service/internal/preprocessor/partition/partition.go
@@ -10,13 +10,26 @@ type Partition struct {
 	Levels []int
 }
 
-func (p *Partition) LevelsToString(maxPartitions int) string {
+func (p *Partition) LevelsToString(maxPartitions int) (string, error) {
+	if maxPartitions < 1 {
+		return "", fmt.Errorf("Could not parse levels to string with negative partitions: %d", maxPartitions)
+	}
+
 	result := make([]string, len(p.Levels))
 	for i := 0; i < len(p.Levels); i++ {
+		if p.Levels[i] > maxPartitions {
+			return "", fmt.Errorf("Can't parse levels to string if the number of partitions is lower than level %d", p.Levels[i])
+		}
+
+		if p.Levels[i] < 0 {
+			return "", fmt.Errorf("Can't parse negative level to string %d", p.Levels[i])
+		}
+
 		format := fmt.Sprintf("%%0%dd", getNumberWidth(maxPartitions))
 		result[i] = fmt.Sprintf(format, p.Levels[i])
 	}
-	return strings.Join(result, "/")
+
+	return strings.Join(result, "/"), nil
 }
 
 func getNumberWidth(n int) int {
--- service/internal/preprocessor/partition/partition_test.go
+++ service/internal/preprocessor/partition/partition_test.go
@@ -10,22 +10,31 @@ type ToStringTestCase struct {
 	Input         []int
 	Expected      string
 	NumPartitions int
+	Error         bool
 }
 
 func TestPartition_ToString(t *testing.T) {
 	cases := []ToStringTestCase{
-		{[]int{10, 88}, "010/088", 256},
-		{[]int{10, 88}, "10/88", 64},
-		{[]int{10, 88}, "0010/0088", 1024},
-		{[]int{0, 24}, "00/24", 32},
-		{[]int{0, 0}, "00/00", 32},
-		{[]int{0, 1}, "0/1", 8},
+		{[]int{10, 88}, "010/088", 256, false},
+		{[]int{10, 50}, "10/50", 64, false},
+		{[]int{10, 88}, "0010/0088", 1024, false},
+		{[]int{0, 24}, "00/24", 32, false},
+		{[]int{0, 0}, "00/00", 32, false},
+		{[]int{0, 1}, "0/1", 8, false},
+		{[]int{-1, -1}, "", 2, true},
+		{[]int{1, 1}, "", -1, true},
+		{[]int{10, 10}, "", 1, true},
 	}
 
 	for i := 0; i < len(cases); i++ {
 		part := Partition{Levels: cases[i].Input}
-		result := part.LevelsToString(cases[i].NumPartitions)
+		result, err := part.LevelsToString(cases[i].NumPartitions)
 
+		if cases[i].Error {
+			require.Error(t, err, "Case %d", i)
+		} else {
+			require.NoError(t, err, "Case %d", i)
+		}
 		require.Equal(t, cases[i].Expected, result, "Case %d. %v", i, cases[i])
 	}
 }
