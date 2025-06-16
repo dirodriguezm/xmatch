ba85f7ce9c3a4766d7db312568d65328da2c0121
matisin <matias.medina.s2@gmail.com>
2025-06-16 16:59:48 -0400

    removed comment and added license text

--- service/internal/web/helpers_test.go
+++ service/internal/web/helpers_test.go
@@ -1,3 +1,16 @@
+// Copyright 2024-2025 Matías Medina Silva
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package web
 
 import (
@@ -11,28 +24,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// func (web *Web) render(c *gin.Context, status int, page string, data templateData) {
-// ts, ok := web.templateCache[page]
-// if !ok {
-// err := fmt.Errorf("the template %s does not exist", page)
-// web.serverError(c, err)
-// return
-// }
-
-// buf := new(bytes.Buffer)
-// if err := ts.ExecuteTemplate(buf, "base", data); err != nil {
-// web.serverError(c, err)
-// return
-// }
-
-// c.Data(status, "text/html; charset=utf-8", buf.Bytes())
-// }
-
-// func TestRender(t *testing.T) {
-// tests := []struct {
-// name string
-// }{}
-// }
 func TestServerError(t *testing.T) {
 	// Setup
 	stdout := &strings.Builder{}
