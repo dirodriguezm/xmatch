fix resolution calculation

create_ipix.py
@@ -8,9 +8,10 @@ import random
 NSIDE = None
 ITERATIONS = 1
 
+
 def main():
-    global NSIDE 
-    NSIDE = 2**int(sys.argv[2])
+    global NSIDE
+    NSIDE = 2 ** int(sys.argv[2])
     global ITERATIONS
     ITERATIONS = int(sys.argv[3])
 
@@ -39,6 +40,7 @@ def test_ang2pix():
     print("results:", len(results))
     print(results[:10])
 
+
 def test_conesearch():
     radius = np.deg2rad(10 / 3600.0)
     results = []
@@ -52,6 +54,7 @@ def test_conesearch():
     print("results:", len(results))
     print(results[0])
 
+
 def test_conesearch2():
     radius = np.deg2rad(10 / 3600.0)
     for _ in range(0, ITERATIONS):
@@ -59,6 +62,7 @@ def test_conesearch2():
             for dec in range(-90, 90):
                 vec = hp.ang2vec(ra, dec, lonlat=True)
                 hp.query_disc(NSIDE, vec, radius, inclusive=True, nest=True)
+
 
 def test_conesearch_single_op():
     radius = np.deg2rad(10 / 3600.0)
@@ -72,11 +76,11 @@ def test_conesearch_single_op():
 def radec_to_thetaphi(ra, dec):
     """
     Convert RA and Dec in degrees to HEALPix theta and phi in radians.
-    
+
     Parameters:
     ra (float): Right Ascension in degrees
     dec (float): Declination in degrees
-    
+
     Returns:
     tuple: (theta, phi) in radians
     """
@@ -84,26 +88,29 @@ def radec_to_thetaphi(ra, dec):
     phi = np.radians(ra)
     return theta, phi
 
+
 def nside_to_resolution(nside):
-    value = 2**int(nside)
+    value = 2 ** int(nside)
     print(
-        "Approximate resolution at NSIDE {} is {:.2} arcsec".format(
-            nside, hp.nside2resol(value, arcmin=True) / 60
+        "Approximate resolution at NSIDE {} is {:.3} arcsec".format(
+            nside, hp.nside2resol(value, arcmin=True) * 60
         )
     )
+
 
 def plot_resolutions():
     resolutions = []
     nsides = []
-    for nside in range(12, 18):
+    for nside in range(12, 19):
         nsides.append(nside)
-        value = 2**int(nside)
-        resolution = hp.nside2resol(value, arcmin=True) / 60
+        value = 2 ** int(nside)
+        resolution = hp.nside2resol(value, arcmin=True) * 60
         resolutions.append(resolution)
     plt.plot(nsides, resolutions)
     plt.xlabel("NSIDE")
     plt.ylabel("Resolution (arcsec)")
     plt.show()
+
 
 if __name__ == "__main__":
     main()